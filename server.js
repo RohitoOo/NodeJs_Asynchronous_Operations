1ac968469307cf0c808a09b9405b877ad1166a65
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-14 20:45:31 +0530

    Basic Routing NodeJs. Client & Server Communication.

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const http = require('http')
+const fs = require('fs')
+
 
 var server = http.createServer( (req,res) => {
 console.log("Request was made to : " , req.url)
-res.writeHead(200, {'Content-type' : 'text/plain'})
-res.end("Server responsed!")
+
+if( req.url == "/website" ) {
+
+  res.writeHead(200, {'Content-type' : 'text/html'})
+  var myReadStream = fs.createReadStream(__dirname + "/index.html" , "utf8" )
+  myReadStream.pipe(res);
+
+} else {
+  res.writeHead(200, {'Content-type' : 'text/plain'})
+
+  res.end("Home Page")
+}
+
 })
 
 server.listen(3000, 'localhost')
